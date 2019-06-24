29a81a5b244876bf391b582c72de0c4fdd2624bd
Brenno <brenno_silva_@hotmail.com>
2019-06-24 17:19:37 -0300

    inicio implementação do torrentMaker

--- src/client/TorrentMaker.java
+++ src/client/TorrentMaker.java
@@ -1,6 +1,7 @@
 package client;
 
 import java.io.File;
+import java.io.RandomAccessFile;
 
 public class TorrentMaker {
 	
@@ -13,11 +14,38 @@ public class TorrentMaker {
 	public void makeTorrent() {
 		
 		try {
-			String[] splitFileName = this.file.getName().split(".");
-			File f = new File("/home/brenno/Documentos/"+splitFileName[0]+".torrent");
-		}
-		catch(Exception e) {
+			File f = new File("/home/brenno/Documentos/"+getNameOnTorrentFormat());
+			RandomAccessFile torrent = new RandomAccessFile(f,"w");
 			
+			RandomAccessFile file = new RandomAccessFile(this.file,"r");
+			
+			torrent.writeBytes(getNameWithoutExtension());
+			
+		}	
+		catch(Exception e) {
+			System.out.println("Erro ao gerar arquivo .torrent: "+e.getMessage());
 		}
+	}
+	
+	private int getNumberOfPeças(int size) {
+		long bytes = this.file.length();
+		
+		if((bytes%size) == 0) {
+			return 0;
+		}
+		else {
+			return 0;
+		}
+	}
+	
+	
+	
+	private String getNameWithoutExtension() {
+		return this.file.getName().split(".")[0];
+	}
+	
+	private String getNameOnTorrentFormat() {
+		String name = this.file.getName().split(".")[0];	
+		return name+".torrent";
 	}
 }
